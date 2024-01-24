5b29dd7aaf829a217e3165090aa604cfb7ee46b6
Mehmet Temizkan <56386597+mehmettemizkan@users.noreply.github.com>
2024-01-24 19:12:43 +0300

    isItPrime App Updated.

--- isItPrime/src/isItPrime/Main.java
+++ isItPrime/src/isItPrime/Main.java
@@ -10,32 +10,34 @@ public class Main {
 		System.out.println("This program finds the prime numbers.");
 
 		Scanner myNumber = new Scanner(System.in);
-		System.out.println("Enter a number greater than 1");
-
-		double userNumber = myNumber.nextDouble();
-		double userNumberSquare = Math.sqrt(userNumber);
-		int userNumberSquareInteger = (int) Math.ceil(userNumberSquare);
-
-		int numberOfDivisors = 0;
-
-		if (userNumber < 2) {
-			System.err.println("Invalid number! The number must be greater than 1");
-		} else {
-			if (userNumber == (int) userNumber) { // if userNumber is an integer
+		System.out.println("Enter an integer number greater than 1");
+		
+		if (myNumber.hasNextInt()) {
+			
+			int userNumber = myNumber.nextInt();
+			myNumber.close();
+	
+			double userNumberSquare = Math.sqrt(userNumber);
+			int userNumberSquareInteger = (int) Math.ceil(userNumberSquare);
+	
+			int count = 0;
+	
+			if (userNumber < 2) {
+				System.err.println("Invalid number! The number must be greater than 1");
+			} else {
 				for (int i = 2; i <= userNumberSquareInteger; i++) {
-					int remainder = (int) userNumber % i;
-					if (remainder == 0) {
-						numberOfDivisors += 1;
+					if (userNumber % i == 0) {
+						System.out.println(userNumber + " is not a prime number.");
+						count++;
+						break;
 					}
 				}
-				if (numberOfDivisors == 0) {
+				if (count == 0) {
 					System.out.println((int) userNumber + " is a prime number.");
-				} else {
-					System.out.println((int) userNumber + " is not a prime number.");
 				}
-			} else {
-				System.err.println("Invalid number! The number must be an integer.");
 			}
+		}else {
+			System.err.println("Invalid input! Please enter an integer greater than 1.");
 		}
 	}
 }
